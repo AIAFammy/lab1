914b6bdf0a1629427c250a6c899f240e56525cb9
C14011277 <ming830710@gmail.com>
2016-03-03 19:15:10 +0800

    lab1 commit 1.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lab1
+
+this is README file for lab1.
+
+program is name "lab1.cpp"
+
+compiled by "g++ -o lab1 lab1.cpp"
+
+runned by "./lab1"
--- lab1.cpp
+++ lab1.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-  cout<<"hello"<<endl;
+  int n;
+  cout<<"input n:";
+  cin>>n;
+  while(1)
+  {
+    cout<<n<<" ";
+    if(n == 1) break;
+    else if(n%2 == 1) n = 3*n+1;
+    else n = n/2;
+  }
+  cout<<endl;
   return 0;
 }
